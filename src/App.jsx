55354ebd3012858886df380b9b422a8a9a528fab
NaingWinAung <naingwinaung1710@gmail.com>
2025-09-03 22:53:30 +0700

    project set up and nav bar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar"
+const App = () => {
+  return (
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+      {/* Navbar */}
+      <Navbar/>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Id doloribus inventore voluptates iure beatae dolorem deleniti ipsa, ab atque? Voluptatem ipsa voluptatum libero recusandae. Odit fuga recusandae ratione illum beatae.
+      {/* breadcrumb */}
+      {/* Introduction */}
+      {/* featured post */}
+      {/* post list */}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,60 @@
+import { Menu } from "lucide-react";
+import { X } from "lucide-react";
+import { LogIn } from "lucide-react";
+
+import { useState } from "react";
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="w-full h-16 md:h-20 flex items-center justify-between">
+      {/* LOGO */}
+      <div className="flex items-center gap-4 text-2xl font-medium">
+        <img src="/logo.png" alt="Logo" className="w-8 h-8" />
+        <span>lamalog</span>
+      </div>
+      {/* Mobile menu */}
+      <div className="md:hidden">
+        <div
+          className="cursor-pointer"
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          {open ? <X /> : <Menu />}
+        </div>
+
+        {/* Mobile link list */}
+        <div
+          className={`w-full h-screen flex flex-col gap-10 items-center pt-25 absolute top-16 bg-white/10 backdrop-filter backdrop-blur-sm transition-all ease-in-out duration-200 select-none font-medium
+            ${open ? "-right-0" : "-right-full"}
+          `}
+        >
+          <a href="/">Home</a>
+          <a href="/">Trending</a>
+          <a href="/">Most Popular</a>
+          <a href="/">About</a>
+          <a href="/">
+            <button className="flex gap-1 items-center px-4 py-2 rounded-[2rem] bg-blue-600 text-white cursor-pointer">
+              Log in
+              <LogIn size={22} />
+            </button>
+          </a>
+        </div>
+      </div>
+      {/* Desktop menu */}
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <a href="/">Home</a>
+        <a href="/">Trending</a>
+        <a href="/">Most Popular</a>
+        <a href="/">About</a>
+        <a href="/">
+          <button className="flex gap-1 items-center px-4 py-2 rounded-[2rem] bg-blue-600 text-white cursor-pointer">
+            Log in
+            <LogIn size={22} />
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
